c12185b4f74d9436729bc438c83ab588e9385968
Anders Lassen <49290462+anlas17@users.noreply.github.com>
2022-01-04 11:11:35 +0100

    Add files via upload

---
+++ src/models/predict_model.py
@@ -0,0 +1,58 @@
+import argparse
+import sys
+
+import torch
+from torch import nn, optim
+
+from data import mnist
+from model import MyAwesomeModel
+
+import matplotlib.pyplot as plt
+
+def evaluate(self):
+    test_path = "C:/Users/zebra/.cookiecutters/cookiecutter-data-science/{{ cookiecutter.repo_name }}/data/raw/test.npz"
+    test_data = np.load(test_path)
+
+    class TestDataset(Dataset):
+        def __init__(self):
+            self.data = test_data.f.images
+            self.target = test_data.f.labels
+        
+        def __getitem__(self, index):
+            x = self.data[index]
+            y = self.target[index]
+            data_dict={'image': x, 'label': y}
+            return data_dict
+    
+        def __len__(self):
+            return len(self.data)
+    
+    def mnist():
+        test = TestDataset()
+    
+        return test
+    
+    print("Evaluating until hitting the ceiling")
+    parser = argparse.ArgumentParser(description='Training arguments')
+    parser.add_argument('load_model_from', default="")
+    # add any additional argument that you want
+    args = parser.parse_args(sys.argv[2:])
+    print(args)
+        
+    # TODO: Implement evaluation logic here
+    model = MyAwesomeModel()
+    state_dict = torch.load('C:/Users/zebra/.cookiecutters/cookiecutter-data-science/{{ cookiecutter.repo_name }}/models/checkpoint.pth')
+    model.load_state_dict(state_dict)
+    test_set = mnist()
+    testloader = torch.utils.data.DataLoader(test_set, batch_size=64, shuffle=True)
+        
+    with torch.no_grad():
+       for (i, batch) in enumerate(testloader):
+          images, labels = batch["image"], batch["label"]
+          ps = torch.exp(model(images.float()))
+          top_p, top_class = ps.topk(1, dim=1)
+          equals = top_class == labels.view(*top_class.shape)
+          accuracy = torch.mean(equals.type(torch.FloatTensor))
+     print(f'Accuracy: {accuracy.item()*100}%')
+if __name__ == '__main__':
+    evaluate()
