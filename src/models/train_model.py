60618218973b4a3c40f7101b4788282ac4deb995
Anders Lassen <49290462+anlas17@users.noreply.github.com>
2022-01-04 11:35:44 +0100

    added comments and a docstring

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -10,9 +10,11 @@ from model import MyAwesomeModel
 import matplotlib.pyplot as plt
 
 def train(self):
+        # Loading training data
         train_path = "C:/Users/zebra/.cookiecutters/cookiecutter-data-science/{{ cookiecutter.repo_name }}/data/raw/train_0.npz"
         train_data = np.load(train_path)
-
+        
+        # Creating training dataset for torch dataloader
         class TrainDataset(Dataset):
             def __init__(self):
                 self.data = train_data.f.images
@@ -26,23 +28,23 @@ def train(self):
     
             def __len__(self):
                 return len(self.data)
+        
         def mnist():
+            """
+            Returns a Pytorch Dataset created with training data.
+            
+                Returns:
+                        train (Dataset): Training dataset to be used through a dataloader
+            """
             train = TrainDataset()
-    
             return train
-        
-        print("Training day and night")
-        parser = argparse.ArgumentParser(description='Training arguments')
-        parser.add_argument('--lr', default=0.1)
-        # add any additional argument that you want
-        args = parser.parse_args(sys.argv[2:])
-        print(args)
-        
-        # TODO: Implement training loop here
+       
+        # Initializing model, loss and optimizer
         model = MyAwesomeModel()
         criterion = nn.NLLLoss()
         optimizer = optim.Adam(model.parameters(), lr=0.0001)
         
+        # Calling dataset function and creating dataloader
         train_set = mnist()
         trainloader = torch.utils.data.DataLoader(train_set, batch_size=64, shuffle=True)
         
@@ -50,13 +52,14 @@ def train(self):
         steps = 0
 
         train_losses, test_losses = [], []
+        # Training model
         for e in range(epochs):
             running_loss = 0
             for (i, batch) in enumerate(trainloader):
                 images, labels = batch["image"], batch["label"]
                 optimizer.zero_grad()
         
-                log_ps = model(images.float())
+                log_ps = model(images.float()) # Data to float explicitly
                 loss = criterion(log_ps, labels)
                 loss.backward()
                 optimizer.step()
@@ -65,6 +68,7 @@ def train(self):
             train_loss = running_loss/len(trainloader)
             train_losses.append(train_loss)
             print(train_loss)
+        # Plotting the training loss and saving the plot
         plt.plot(train_losses)
         plt.show()
         plt.savefig('C:/Users/zebra/.cookiecutters/cookiecutter-data-science/{{ cookiecutter.repo_name }}/reports/figures/train_plot.png')
